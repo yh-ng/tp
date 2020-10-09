6fa8975a4e8a20768a59234802b610854cffe22b
iamchenjiajun <iamchenjiajun@gmail.com>
2020-10-10 00:02:05 +0800

    Fix checkstyle violations

--- src/test/java/seedu/duke/task/TodoTest.java
+++ src/test/java/seedu/duke/task/TodoTest.java
@@ -2,7 +2,9 @@ package seedu.duke.task;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class TodoTest {
 
