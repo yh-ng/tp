ec8ed89cc7b9bae2c3293a3784b95b6ac4484806
iamchenjiajun <iamchenjiajun@gmail.com>
2020-10-09 17:06:04 +0800

    Fix checkstyleTest errors in DukeTest

--- src/test/java/seedu/duke/DukeTest.java
+++ src/test/java/seedu/duke/DukeTest.java
@@ -3,15 +3,14 @@ package seedu.duke;
 import org.junit.jupiter.api.Test;
 
 class DukeTest {
+    static final String LOGO = "    ____        _        \n"
+            + "    |  _ \\ _   _| | _____ \n"
+            + "    | | | | | | | |/ / _ \\\n"
+            + "    | |_| | |_| |   <  __/\n"
+            + "    |____/ \\__,_|_|\\_\\___|\n";
+
     @Test
     public void sampleTest() {
-
-        String LOGO ="    ____        _        \n"
-                + "    |  _ \\ _   _| | _____ \n"
-                + "    | | | | | | | |/ / _ \\\n"
-                + "    | |_| | |_| |   <  __/\n"
-                + "    |____/ \\__,_|_|\\_\\___|\n";
-
         System.out.println("Testing\n" + LOGO);
     }
 }
