e4136e1a980398f7fd7c9674a0228ed73fe586ff
iamchenjiajun <iamchenjiajun@gmail.com>
2020-10-09 23:59:36 +0800

    Add JUnit tests for Todo class

---
+++ src/test/java/seedu/duke/task/TodoTest.java
@@ -0,0 +1,45 @@
+package seedu.duke.task;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TodoTest {
+
+    @Test
+    void getDescription_inputDescription_returnsCorrectDescription() {
+        String inputString = "test description";
+        Todo todo = new Todo(inputString);
+        assertEquals(inputString, todo.getDescription());
+    }
+
+    @Test
+    void getIsDone_isDone_returnsFalseByDefault() {
+        Todo todo = new Todo("test description");
+        assertFalse(todo.getIsDone());
+    }
+
+    @Test
+    void markAsDone_setIsDone_todoSetAsDone() {
+        Todo todo = new Todo("test description");
+        assertFalse(todo.getIsDone());
+        todo.markAsDone();
+        assertTrue(todo.getIsDone());
+    }
+
+    @Test
+    void toFile_getToFile_returnsCorrectString() {
+        Todo todo = new Todo("test description");
+        String fileString = todo.toFile();
+        String expectedString = "T | 0 | test description";
+        assertEquals(expectedString, fileString);
+    }
+
+    @Test
+    void testToString_toString_returnsCorrectString() {
+        Todo todo = new Todo("test description");
+        String todoString = todo.toString();
+        String expectedString = "[T][N] test description";
+        assertEquals(expectedString, todoString);
+    }
+}
