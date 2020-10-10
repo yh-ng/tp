fbae7736fb5462551ea161abf801bc616c164573
iamchenjiajun <iamchenjiajun@gmail.com>
2020-10-10 18:15:44 +0800

    Add JUnit tests for new methods in Parser

---
+++ src/test/java/seedu/duke/parser/ParserTest.java
@@ -0,0 +1,27 @@
+package seedu.duke.parser;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ParserTest {
+
+    @Test
+    void getArgumentsFromRegex_validCommand_parseArgumentsCorrectly() {
+        String testCommand = "add tP meeting by/16-09-23:59 at/15-09-2020-11:00 p/1";
+        HashMap<String, String> argumentsMap = Parser.getArgumentsFromRegex(testCommand, Parser.ARGUMENT_REGEX);
+        assertEquals("16-09-23:59", argumentsMap.get("by"));
+        assertEquals("15-09-2020-11:00", argumentsMap.get("at"));
+        assertEquals("1", argumentsMap.get("p"));
+    }
+
+    @Test
+    void removeRegexFromArguments_validCommand_returnsDescription() {
+        String testCommand = "add tP meeting by/16-09-23:59 at/15-09-2020-11:00 p/1";
+        String parsedString = Parser.removeRegexFromArguments(testCommand, Parser.ARGUMENT_REGEX);
+        String expectedString = "add tP meeting";
+        assertEquals(expectedString, parsedString);
+    }
+}
